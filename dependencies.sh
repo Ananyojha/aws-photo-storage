f6ec6702c8ad27ce1ef98f172705ae84abaa0ef1
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2021-12-12 22:42:07 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -7,3 +7,13 @@ npm install -g db-migrate
 npm install db-migrate
 npm install dotenv
 npm install ejs
+npm install jsonwebtoken
+npm install --save multer
+npm install pg
+
+echo ''
+echo 'to install express.js'
+echo 'cd into the app directory'
+echo 'npm init'
+echo 'npm install express --save'
+
